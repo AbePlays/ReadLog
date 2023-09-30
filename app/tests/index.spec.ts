6207caeac59785f0d8f01abc5ff7f3e964f66cf0
Abhishek Rawat <abhi.rawat456@gmail.com>
2023-09-30 18:33:56 +0530

    test: add test for index route

---
+++ app/tests/index.spec.ts
@@ -0,0 +1,8 @@
+import { expect, test } from '@playwright/test'
+
+test('has a link to the books route', async ({ page }) => {
+  await page.goto('/')
+  const booksLink = page.getByRole('link', { name: 'Checkout Popular Books' })
+  await expect(booksLink).toHaveAttribute('href', '/books')
+  await expect(booksLink).toBeInViewport()
+})
