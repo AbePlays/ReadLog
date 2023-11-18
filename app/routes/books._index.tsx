23b8dec7b68e1991a8fc3efedaf1b3aede7bbb0c
Abhishek Rawat <abhi.rawat456@gmail.com>
2023-11-18 17:59:00 +0530

    feat: add book component

--- app/routes/books._index.tsx
+++ app/routes/books._index.tsx
@@ -1,52 +1,51 @@
-import type { LoaderFunctionArgs, MetaFunction } from '@remix-run/cloudflare'
-import { Link, useLoaderData } from '@remix-run/react'
+import type { LoaderFunctionArgs, MetaFunction } from '@remix-run/cloudflare';
+import { Link, useLoaderData } from '@remix-run/react';
 
-import { BooksSchema } from '~/schemas/bookSchema'
+import BookCover from '~/components/BookCover';
+import { BooksSchema } from '~/schemas/bookSchema';
 
 export const meta: MetaFunction = () => {
   return [
     { title: 'Popular Books - ReadLog' },
     {
       name: 'description',
-      content: 'Explore a curated collection of popular books. Find your next great read on our platform.'
-    }
-  ]
-}
+      content:
+        'Explore a curated collection of popular books. Find your next great read on our platform.',
+    },
+  ];
+};
 
 export async function loader({ context }: LoaderFunctionArgs) {
   const res = await fetch(
     `https://www.googleapis.com/books/v1/volumes?q=subject:'fiction','nonfiction','mystery','science','fantasy'&orderBy=relevance&key=${context.env.GOOGLE_BOOKS_API_KEY}`
-  )
-  const data = await res.json()
-  const books = BooksSchema.parse(data)
-  return books
+  );
+  const data = await res.json();
+  const books = BooksSchema.parse(data);
+  return books;
 }
 
 export default function BooksRoute() {
-  const loaderData = useLoaderData<typeof loader>()
+  const loaderData = useLoaderData<typeof loader>();
 
   return (
     <div>
       <h1 className="bg-red-200 text-center p-2" id="popular-books">
         Popular Books
       </h1>
-      <ul aria-labelledby="popular-books">
+      <ul
+        aria-labelledby="popular-books"
+        className="grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] justify-items-center gap-x-12 gap-y-8 p-6"
+      >
         {loaderData.items.map((book) => {
           return (
-            <li key={book.id}>
+            <li className="w-full" key={book.id}>
               <Link to={`./${book.id}`}>
-                <h2>{book.volumeInfo.title}</h2>
-                <img
-                  alt={book.volumeInfo.title}
-                  height="180px"
-                  src={book.volumeInfo.imageLinks?.thumbnail}
-                  width="120px"
-                />
+                <BookCover book={book} />
               </Link>
             </li>
-          )
+          );
         })}
       </ul>
     </div>
-  )
+  );
 }
