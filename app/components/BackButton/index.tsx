4145a50b58f55f03c2528431567859af3479a25e
Abhishek Rawat <abhi.rawat456@gmail.com>
2023-11-20 22:40:07 +0530

    feat: improve styles

--- app/components/BackButton/index.tsx
+++ app/components/BackButton/index.tsx
@@ -1,12 +1,25 @@
 import { useNavigate } from '@remix-run/react'
 import { ArrowLeft } from 'lucide-react'
 
+import { cn } from '~/utils/cn'
+
 export default function BackButton(props: React.ComponentProps<'button'>) {
+  const { className, ...rest } = props
   const navigate = useNavigate()
 
   return (
-    <button aria-label="Go Back" onClick={() => navigate(-1)} type="button" title="Go Back" {...props}>
-      <ArrowLeft aria-hidden="true" />
+    <button
+      aria-label="Go Back"
+      className={cn(
+        'hover:bg-gray-100 rounded-full p-1 transition-[background-color,box-shadow] duration-300 focus-visible:ring-1 ring-black outline-none',
+        className
+      )}
+      onClick={() => navigate(-1)}
+      title="Go Back"
+      type="button"
+      {...rest}
+    >
+      <ArrowLeft aria-hidden="true" strokeWidth={1.5} />
     </button>
   )
 }
