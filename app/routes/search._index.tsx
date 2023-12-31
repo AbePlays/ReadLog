1e1624a6307b175be87164a5b5f89cffcfe88e40
Abhishek Rawat <abhi.rawat456@gmail.com>
2023-12-31 18:23:39 +0530

    feat: add pagination on search

--- app/routes/search._index.tsx
+++ app/routes/search._index.tsx
@@ -1,10 +1,11 @@
 import type { LoaderFunctionArgs, MetaFunction } from '@remix-run/cloudflare'
-import { Form, Link, useLoaderData, useNavigation, useSearchParams } from '@remix-run/react'
-import { Book, Search, SearchX } from 'lucide-react'
+import { Form, useLoaderData, useNavigation, useSearchParams } from '@remix-run/react'
+import { Book, MoveLeft, MoveRight, Search, SearchX } from 'lucide-react'
 
 import { BookCover } from '~/components/book-cover'
 import { Spinner } from '~/components/spinner'
 import { Button } from '~/components/ui/button'
+import { Link } from '~/components/ui/link'
 import { Select } from '~/components/ui/select'
 import { TextField } from '~/components/ui/text-field'
 import { BooksSchema } from '~/schemas/book'
@@ -13,10 +14,7 @@ import { cn } from '~/utils/cn'
 export const meta: MetaFunction = () => {
   return [
     { title: 'Book Search - ReadLog' },
-    {
-      name: 'description',
-      content: 'Search for books, discover new titles, and find your favorite reads online'
-    }
+    { name: 'description', content: 'Search for books, discover new titles, and find your favorite reads online' }
   ]
 }
 
@@ -25,10 +23,11 @@ export async function loader({ context, request }: LoaderFunctionArgs) {
 
   const query = url.searchParams.get('q')
   const genre = url.searchParams.get('genre') === 'all' ? '' : url.searchParams.get('genre')
+  const page = (Number(url.searchParams.get('page') ?? '1') - 1) * 10
 
   if (query) {
     const response = await fetch(
-      `https://www.googleapis.com/books/v1/volumes?q=intitle:${query},subject:${genre}&key=${context.env.GOOGLE_BOOKS_API_KEY}`
+      `https://www.googleapis.com/books/v1/volumes?q=intitle:${query},subject:${genre}&startIndex=${page}&maxResults=10&key=${context.env.GOOGLE_BOOKS_API_KEY}`
     )
     const data = await response.json()
     const books = BooksSchema.parse(data)
@@ -39,13 +38,19 @@ export async function loader({ context, request }: LoaderFunctionArgs) {
 }
 
 export default function SearchRoute() {
-  const loaderData = useLoaderData<typeof loader>()
-  const { location, state } = useNavigation()
   const [searchParams] = useSearchParams()
+  const { location, state } = useNavigation()
+  const loaderData = useLoaderData<typeof loader>()
 
   const isLoadingBooks = state === 'loading' && location.pathname === '/search'
   const query = searchParams.get('q') ?? ''
   const genre = searchParams.get('genre') ?? 'all'
+  const page = Number(searchParams.get('page') ?? '1')
+
+  const pagination = {
+    isPreviousDisabled: page === 1,
+    isNextDisabled: (loaderData?.items?.length ?? 0) < 10
+  }
 
   return (
     <>
@@ -82,39 +87,58 @@ export default function SearchRoute() {
       </Form>
 
       {loaderData?.totalItems === 0 ? (
-        <div
-          className={cn('grid place-items-center my-6', {
-            'pointer-events-none opacity-30': isLoadingBooks
-          })}
-        >
+        <div className={cn('grid place-items-center my-6', { 'pointer-events-none opacity-30': isLoadingBooks })}>
           <SearchX aria-hidden="true" className="text-gray-200" size={160} />
           <span className="block text-lg font-medium mt-4">No Results Found</span>
           <p className="text-gray-600">Try searching for a different book or genre.</p>
         </div>
-      ) : (
-        <ul
-          aria-label="Search Results"
-          className={cn(
-            'grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] justify-items-center gap-x-12 gap-y-8 p-6',
-            {
-              'pointer-events-none opacity-30': isLoadingBooks
-            }
-          )}
-        >
-          {loaderData?.items?.map((book) => {
-            return (
-              <li className="w-full" key={book.id}>
-                <Link
-                  className="outline-none block rounded-lg h-full transition duration-300 focus-visible:ring-2 ring-offset-2 ring-gray-500"
-                  to={`/books/${book.id}`}
-                >
-                  <BookCover book={book} />
-                </Link>
-              </li>
-            )
-          })}
-        </ul>
-      )}
+      ) : null}
+
+      {(loaderData?.items?.length ?? 0) > 0 ? (
+        <div className={cn({ 'pointer-events-none opacity-30': isLoadingBooks })}>
+          <ul
+            aria-label="Search Results"
+            className="grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] justify-items-center gap-x-12 gap-y-8 p-6"
+          >
+            {loaderData?.items?.map((book) => {
+              return (
+                <li className="w-full" key={book.id}>
+                  <Link className="rounded-lg h-full" to={`/books/${book.id}`}>
+                    <BookCover book={book} />
+                  </Link>
+                </li>
+              )
+            })}
+          </ul>
+
+          {/* https://github.com/evdhiggins/book-inquiry?tab=readme-ov-file#totalitems-and-calculating-pagination */}
+          <nav aria-label="Pagination" className="flex gap-4 items-center justify-center mt-4">
+            <Link
+              aria-label={`Go to Page ${page - 1}`}
+              className={cn({ 'pointer-events-none opacity-30': pagination.isPreviousDisabled })}
+              disabled={pagination.isPreviousDisabled}
+              to={`/search?q=${query}&genre=${genre}&page=${page - 1}`}
+            >
+              <MoveLeft size={20} />
+            </Link>
+
+            <div className="flex gap-4 items-center">
+              <hr className="w-8 border-gray-300" />
+              <span className="font-medium">Page {page}</span>
+              <hr className="w-8 border-gray-300" />
+            </div>
+
+            <Link
+              aria-label={`Go to Page ${page + 1}`}
+              className={cn({ 'pointer-events-none opacity-30': pagination.isNextDisabled })}
+              disabled={pagination.isNextDisabled}
+              to={`/search?q=${query}&genre=${genre}&page=${page + 1}`}
+            >
+              <MoveRight size={20} />
+            </Link>
+          </nav>
+        </div>
+      ) : null}
     </>
   )
 }
