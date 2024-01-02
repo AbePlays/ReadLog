c744e48949dd98d682c974e79965ee259702db3c
Abhishek Rawat <abhi.rawat456@gmail.com>
2024-01-02 22:00:19 +0530

    feat: add fixed height to components

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,11 @@ import type { Config } from 'tailwindcss'
 export default {
   content: ['./app/**/*.{ts,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      height: {
+        base: '2.5rem'
+      }
+    }
   },
   plugins: [require('@tailwindcss/typography')]
 } satisfies Config
