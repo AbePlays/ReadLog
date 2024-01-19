7e6bafa557af9b0b8bc8dfcc1c1aa16e91b656fb
Abhishek Rawat <abhi.rawat456@gmail.com>
2024-01-19 22:01:50 +0530

    test: add genre tests

--- app/tests/books._index.spec.ts
+++ app/tests/books._index.spec.ts
@@ -5,6 +5,20 @@ test('has Popular Books heading', async ({ page }) => {
   await expect(page.getByRole('heading', { name: 'Popular Books' })).toBeVisible()
 })
 
+test('has genre toggle', async ({ page }) => {
+  await page.goto('/books')
+
+  const genreToggle = page.getByRole('group', { name: 'Genre selection' })
+  const genreToggleItems = genreToggle.getByRole('radio')
+  expect(await genreToggleItems.count()).toBe(6)
+
+  const fantasyToggle = genreToggle.getByRole('radio', { name: 'fantasy' })
+  await expect(fantasyToggle).toHaveAttribute('data-state', 'off')
+  await fantasyToggle.click()
+  await expect(fantasyToggle).toHaveAttribute('data-state', 'on')
+  expect(page.url()).toContain('?genre=fantasy')
+})
+
 test('has book title and image in each book in the list', async ({ page }) => {
   await page.goto('/books')
   const booksList = page.getByRole('list', { name: 'Popular Books' })
