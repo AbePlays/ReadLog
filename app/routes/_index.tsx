8b51e7858049654bb03f70e95db43ad48ad27fb1
Abhishek Rawat <abhi.rawat456@gmail.com>
2024-04-01 22:58:17 +0530

    feat: add charts

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,16 @@
 import type { LoaderFunctionArgs, MetaFunction } from '@remix-run/cloudflare'
 import { json, useLoaderData } from '@remix-run/react'
+import { BarChart, LineChart } from '@tremor/react'
 import { jsonWithError } from 'remix-toast'
+
+const chartdata = [
+  { date: 'Jan 22', SemiAnalysis: 2890 },
+  { date: 'Feb 22', SemiAnalysis: 2756 },
+  { date: 'Mar 22', SemiAnalysis: 3322 },
+  { date: 'Apr 22', SemiAnalysis: 3470 },
+  { date: 'May 22', SemiAnalysis: 3475 },
+  { date: 'Jul 22', SemiAnalysis: 3490 }
+]
 
 import { getDbClient } from '~/libs/db/index.server'
 import { getUserId } from '~/utils/session.server'
@@ -54,6 +64,46 @@ export default function IndexRoute() {
           <dd className="block text-3xl">186 mins.</dd>
         </div>
       </dl>
+
+      <div className="mt-6 border rounded-lg p-8">
+        <h2 className="font-medium">Pages read</h2>
+        <LineChart
+          categories={['SemiAnalysis']}
+          className="h-80 mt-8"
+          colors={['orange-300']}
+          curveType="natural"
+          data={chartdata}
+          index="date"
+          showLegend={false}
+        />
+      </div>
+
+      <div className="mt-6 flex gap-6">
+        <div className="border rounded-lg p-8 w-full">
+          <h2 className="font-medium">Longest streak</h2>
+          <BarChart
+            categories={['SemiAnalysis']}
+            className="mt-8"
+            colors={['orange-300']}
+            data={chartdata}
+            index="date"
+            showLegend={false}
+            yAxisWidth={48}
+          />
+        </div>
+        <div className="border rounded-lg p-8 w-full">
+          <h2 className="font-medium">Time spent reading</h2>
+          <BarChart
+            categories={['SemiAnalysis']}
+            className="mt-8"
+            colors={['orange-300']}
+            data={chartdata}
+            index="date"
+            showLegend={false}
+            yAxisWidth={48}
+          />
+        </div>
+      </div>
     </div>
   )
 }
